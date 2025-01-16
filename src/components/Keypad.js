b40f478786e9982930a63e708620e2e2e87e12ca
Stiveys <stevomwaura@gmail.com>
2025-01-16 14:12:46 +0300

    new

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
-// Code Keypad Component Here
+// src/components/Keypad.js
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  const handleChange = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
+};
 
 export default Keypad;
